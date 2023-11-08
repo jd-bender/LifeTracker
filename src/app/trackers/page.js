cc99ba67358cbebbf42885c010f98650afb7dae5
Jake Bender <jdbender64@gmail.com>
2023-11-07 21:19:29 -0800

    fixes data grab

--- src/app/trackers/page.js
+++ src/app/trackers/page.js
@@ -1,30 +1,43 @@
+"use client";
+import { useState, useEffect } from "react";
 import Link from "next/link";
 import { List, ListItem, ListItemButton, ListItemText } from "@mui/material";
-import { getDataFromCollection } from "../../firebase/firestore/getData";
+
 import BackButton from "../../ui/BackButton";
+import RouteConcealer from "../../ui/RouteConcealer";
+import { useAuthContext } from "../../context/AuthContext";
+import { getDataFromCollection } from "../../firebase/firestore/getData";
 import { blueButton } from "../../ui/styles";
 
-const TrackersPage = async () => {
-    const trackers = await getDataFromCollection("trackers");
+const TrackersPage = () => {
+    const { user } = useAuthContext();
+    const [trackers, setTrackers] = useState([]);
 
-    const trackerListItems = trackers.data.map((tracker) =>
-        <ListItem key={tracker.id}>
-            <Link href={`/trackers/${tracker.id}`}  className={`${blueButton} w-48`}>
-                <ListItemButton>
-                    <ListItemText className="text-center" primary={tracker.name} />
-                </ListItemButton>
-            </Link>
-        </ListItem>
-    );
+    useEffect(() => {
+        (async () => {
+            const trackersData = await getDataFromCollection(`users/${user.uid}/trackers`);
+            setTrackers(trackersData.data);
+        })();
+    }, []);
     
     return (
-        <span className="flex">
+        <RouteConcealer isProtected={true} className="flex">
             <List className="h-96 overflow-y-scroll">
-                {trackerListItems}
+                {
+                    trackers.map((tracker) =>
+                        <ListItem key={tracker.id}>
+                            <Link href={`/trackers/${tracker.id}`} className={`${blueButton} w-48`}>
+                                <ListItemButton>
+                                    <ListItemText className="text-center" primary={tracker.name} />
+                                </ListItemButton>
+                            </Link>
+                        </ListItem>
+                    )
+                }
             </List>
 
             <BackButton />
-        </span>
+        </RouteConcealer>
     );
 };
 
