596d9b4c31ff54825b6d166ad19de9b8faa68253
Jake Bender <jdbender64@gmail.com>
2023-11-09 19:16:45 -0800

    converts RootLayout to arrow function

--- src/app/(protectedLayout)/layout.js
+++ src/app/(protectedLayout)/layout.js
@@ -2,18 +2,18 @@ import '../../globals.css';
 import Providers from '../../context/Providers';
 import HeaderBar from './HeaderBar';
 
-export default function RootLayout({ children }) {
-    return (
-        <html lang="en">
-            <body className="bg-teal-300">
-                <Providers>
-                    <HeaderBar />
-                    
-                    <span className="grid h-screen place-items-center">
-                        {children}
-                    </span>
-                </Providers>
-            </body>
-        </html>
-    )
-}
+const RootLayout = ({ children }) => (
+    <html lang="en">
+        <body className="bg-teal-300">
+            <Providers>
+                <HeaderBar />
+                
+                <span className="grid h-screen place-items-center">
+                    {children}
+                </span>
+            </Providers>
+        </body>
+    </html>
+);
+
+export default RootLayout;
