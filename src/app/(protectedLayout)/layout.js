c356a582525f8a04b842f8e32966ed1736365d26
Jake Bender <jdbender64@gmail.com>
2023-11-09 17:30:55 -0800

    improves header bar

--- src/app/(protectedLayout)/layout.js
+++ src/app/(protectedLayout)/layout.js
@@ -1,18 +1,35 @@
 import '../../globals.css';
 import Providers from '../../context/Providers';
-import HeaderBar from './HeaderBar';
+import { AppBar, Toolbar, Typography, IconButton } from '@mui/material';
+import { AccountCircle } from "@mui/icons-material";
 
 export default function RootLayout({ children }) {
     return (
         <html lang="en">
             <body className="bg-teal-300">
-                <span className="grid h-screen place-items-center">
-                    <Providers>
-                        <HeaderBar />
-                        
+                <Providers>
+                    <AppBar>
+                        <Toolbar>
+                            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>Life Tracker</Typography>
+
+                            <span>
+                                <IconButton
+                                    size="large"
+                                    aria-label="account of current user"
+                                    aria-controls="menu-appbar"
+                                    aria-haspopup="true"
+                                    color="inherit"
+                                >
+                                    <AccountCircle />
+                                </IconButton>
+                            </span>
+                        </Toolbar>
+                    </AppBar>
+                    
+                    <span className="grid h-screen place-items-center">
                         {children}
-                    </Providers>
-                </span>
+                    </span>
+                </Providers>
             </body>
         </html>
     )
