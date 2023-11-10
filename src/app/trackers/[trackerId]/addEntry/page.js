e440fc7af16186b704d2d065115ed7c37bd23ca0
Jake Bender <jdbender64@gmail.com>
2023-11-09 16:40:31 -0800

    adds entry viewing

--- src/app/trackers/[trackerId]/addEntry/page.js
+++ src/app/trackers/[trackerId]/addEntry/page.js
@@ -3,6 +3,8 @@ import { useState } from "react";
 import { TextField } from "@mui/material";
 import { useAuthContext } from "../../../../context/AuthContext";
 import { addDocumentWithoutId } from "../../../../firebase/firestore/addData";
+import BackButton from "../../../../ui/BackButton";
+import { blueButton } from "../../../../ui/styles";
 
 const AddEntryPage = ({ params }) => {
     const [entryContents, setEntryContents] = useState("");
@@ -15,7 +17,8 @@ const AddEntryPage = ({ params }) => {
     return (
         <>
             <TextField label="Entry Contents" sx={{width: '20rem'}} onChange={(e) => setEntryContents(e.target.value)} variant="outlined" />
-            <button onClick={saveEntry}>Save</button>
+            <button onClick={saveEntry} className={blueButton}>Save</button>
+            <BackButton backLocation={`trackers/${params.trackerId}`} />
         </>
     );
 };
--- src/app/trackers/[trackerId]/entries/page.js
+++ src/app/trackers/[trackerId]/entries/page.js
@@ -1,5 +1,42 @@
-const EntriesPage = () => {
+"use client";
+import { useState, useEffect } from "react";
+import { List, ListItem, ListItemButton, ListItemText } from "@mui/material";
+import BackButton from "../../../../ui/BackButton";
+import { useAuthContext } from "../../../../context/AuthContext";
+import { getDataFromCollection } from "../../../../firebase/firestore/getData";
+import { blueButton } from "../../../../ui/styles";
+import Link from "next/link";
 
+const EntriesPage = ({ params }) => {
+    const [entries, setEntries] = useState([]);
+    const {user} = useAuthContext();
+
+    useEffect(() => {
+        (async () => {        
+            const entriesSnapshot = await getDataFromCollection(`users/${user.uid}/trackers/${params.trackerId}/entries`);
+            setEntries(entriesSnapshot.data);
+        })();
+    }, []);
+
+    return (
+        <>
+            <List className="h-96 overflow-y-scroll">
+                {
+                    entries.map((entry) =>
+                        <ListItem key={entry.id}>
+                            <Link href={`/trackers/${params.trackerId}/entries/${entry.id}`} className={`${blueButton} w-48`}>
+                                <ListItemButton>
+                                    <ListItemText className="text-center" primary={entry.contents} />
+                                </ListItemButton>
+                            </Link>
+                        </ListItem>
+                    )
+                }
+            </List>
+
+            <BackButton backLocation={`trackers/${params.trackerId}`} />
+        </>
+    );
 };
 
 export default EntriesPage;
